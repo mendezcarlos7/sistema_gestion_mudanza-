fdbabd28b2db420fc39cdcb67cd2f606e70a20a8
mendezcarlos7 <mendezcarlos2028@gmail.com>
2026-09-09 14:24:48 -0500

    Crear clase Cliente

---
+++ src/main/java/model/Cliente.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Cliente {
+
+    private int id;
+    private String nombre;
+    private String telefono;
+    private String correo;
+
+    public Cliente(int id, String nombre, String telefono, String correo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+}
