991dbbdf416ff74a6cca2bd27128fce512c5c9f1
Mrs-natiss <nathaliafanador13@gmail.com>
2026-09-11 23:45:02 -0500

    Crear clase Direccion

---
+++ src/main/java/model/Direccion.java
@@ -0,0 +1,51 @@
+package model;
+
+public class Direccion {
+
+    private String calle;
+    private String numero;
+    private String ciudad;
+    private String codigoPostal;
+
+    public Direccion() {
+    }
+
+    public Direccion(String calle, String numero, String ciudad, String codigoPostal) {
+        this.calle = calle;
+        this.numero = numero;
+        this.ciudad = ciudad;
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+}
